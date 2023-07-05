adc5d6da1926f93244d032b0812ac81e2ff3fd1c
Matheus Motta <matheusdomingues423@gmail.com>
2023-07-05 09:18:12 -0300

    app

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { styled } from "nativewind";
+import { api } from "../src/lib/api";
 import { useEffect } from "react";
 import { useRouter } from "expo-router";
 import { TouchableOpacity, View, Text, Image } from "react-native";
@@ -7,27 +7,35 @@ import Logo from "../src/components/Logo";
 import GoogleLogo from '../src/assets/google.svg'
 import SignInForm from "../src/components/signIn/SignInForm";
 
-import * as AuthSession from 'expo-auth-session'
+import * as SecureStore from 'expo-secure-store'
 import * as Google from 'expo-auth-session/providers/google'
 
 export default function App(){
 
   const router = useRouter()
 
-  // const [, response, signInWithGoogle] = Google.useAuthRequest({
-  //   clientId: '75442975614-g3gdhejorkvi38j663id9n90844a31ob.apps.googleusercontent.com',
-  //   redirectUri: 'https://auth.expo.io/@matheus_motta_18/study.io-frontend',
-  //   scopes: ['profile', 'email']
-  // })
+  const [, response, signInWithGoogle] = Google.useAuthRequest({
+    clientId: '75442975614-g3gdhejorkvi38j663id9n90844a31ob.apps.googleusercontent.com',
+    redirectUri: 'https://auth.expo.io/@matheus_motta_18/study.io-frontend',
+    scopes: ['profile', 'email']
+  })
 
-  // async function handleGoogleToken(access_token: string){
-    
-  // }
+  async function handleGoogleToken(code: string){
+    const response = await api.post('/register', { code })
 
-  // useEffect(() => {
-    
-  // }, [])
-  
+    const { access_token } = response.data
+
+    await SecureStore.setItemAsync('token', access_token)
+
+    router.push('/home')
+  }
+
+  useEffect(() => {
+    if(response?.type === "success" && response.authentication?.accessToken){
+      handleGoogleToken(response.authentication.accessToken)
+    }
+  }, [])
+
 
   return (
     <View className="flex-1 items-center justify-center bg-green-200 p-4">
@@ -36,7 +44,7 @@ export default function App(){
 
         <SignInForm />
 
-        <TouchableOpacity className="bg-white w-full items-center justify-center space-x-2 p-2 flex-row" onPress={() => {}}>
+        <TouchableOpacity className="bg-white w-full items-center justify-center space-x-2 p-2 flex-row" onPress={() => signInWithGoogle()}>
           <GoogleLogo 
             width={20} 
             height={20}
