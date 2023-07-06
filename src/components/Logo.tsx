430211c3aaa3ad1fbc48b3ae6954c27de9ed239a
Matheus Motta <matheusdomingues423@gmail.com>
2023-07-06 11:11:23 -0300

    components

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 
 const Logo = () => {
   return (
-    <Text className='font-title text-white text-5xl p-3'>
-      Study<Text className='text-black'>.io</Text>
-    </Text>
+    <View className='w-full'>
+      <Text className='font-title text-white text-5xl p-2'>
+        Study <Text className='text-black'>.io</Text>
+      </Text>
+
+      <Text className='text-center font-body text-xs text-gray-400'>
+        Sign here and come to see our brand new schedule and goals app!
+      </Text>
+    </View>
   )
 }
 
